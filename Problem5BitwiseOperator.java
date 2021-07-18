de2afb4180faa5a0ecd13b9a6c6b59dd1b688e15
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2021-07-18 18:18:18 +0500

    One Non-Repeating Element
    Find the one non-repeating element in an array where every element repeats thrice

---
+++ Problem5BitwiseOperator.java
@@ -0,0 +1,29 @@
+// Find the one non-repeating element in an array where every element repeats thrice
+
+package problem.pkg5.bitwise.operator;
+
+public class Problem5BitwiseOperator {
+
+    public static void main(String[] args) {
+
+        int a[] = {6, 2, 5, 2, 2, 6, 6};
+        int k = 3;
+
+        byte sizeof_int = 4;
+        int INT_SIZE = 8 * sizeof_int;
+        int count[] = new int[INT_SIZE];
+
+        for (int i = 0; i < INT_SIZE; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if ((a[j] & (1 << i)) != 0) {
+                    count[i] += 1;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < INT_SIZE; i++) {
+            res += (count[i] % k) * (1 << i);
+        }
+        System.out.println(res);
+    }
+}
