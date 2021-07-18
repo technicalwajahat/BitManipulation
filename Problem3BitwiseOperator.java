2bdbcd96c66ae0f5aaedc4947c1fffe5d0bfc80a
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2021-07-18 18:34:37 +0500

    Update Problem3BitwiseOperator.java
    Update package line

--- Problem3BitwiseOperator.java
+++ Problem3BitwiseOperator.java
@@ -1,12 +1,13 @@
 // Find the one non-repeating element in an array where every element repeats twice
 
-package problem.pkg3.bitwise.operator;
+package Problem3BitwiseOperator;
 
 public class Problem3BitwiseOperator {
 
     public static void main(String[] args) {
 
         int[] a = {2, 4, 1, 7, 4, 2, 1};
+        
         int res = 0;
 
         for (int i = 0; i < a.length; i++) {
